8b3d365b105ed5841c22e75b42157c42a07dba40
David <lin.david32@gmail.com>
2022-03-26 20:59:19 -0700

    ui and logic update

--- script.js
+++ script.js
@@ -7,51 +7,75 @@ function formatMove(selection) {
     return selection.charAt(0).toUpperCase() + selection.slice(1);
 }
 
+function outputLog(msg) {
+    var newMsg = document.createElement("p");
+    newMsg.textContent = msg;
+    const log = document.getElementsByClassName("log")[0];
+    log.prepend(newMsg);
+}
+
 function runGame(playerSelection, computerSelection) {
     let selection = playerSelection.toLowerCase()
-    switch(true) {
-        case(selection === "rock" && computerSelection === "rock"):
-            console.log(formatMove(selection) + " vs. " + formatMove(computerSelection) + "! It's a tie!")
+    switch (true) {
+        case (selection === "rock" && computerSelection === "rock"):
+            outputLog(formatMove(selection) + " vs. " + formatMove(computerSelection) + "! It's a tie!")
             return 0
-        case(selection === "rock" && computerSelection === "scissors"):
-            console.log("You win! " + formatMove(selection) + " beats " + formatMove(computerSelection) + "!")
+        case (selection === "rock" && computerSelection === "scissors"):
+            outputLog("You win! " + formatMove(selection) + " beats " + formatMove(computerSelection) + "!")
             return 1
-        case(selection === "scissors" && computerSelection === "scissors"):
-            console.log(formatMove(selection) + " vs. " + formatMove(computerSelection) + "! It's a tie!")
+        case (selection === "scissors" && computerSelection === "scissors"):
+            outputLog(formatMove(selection) + " vs. " + formatMove(computerSelection) + "! It's a tie!")
             return 0
-        case(selection === "scissors" && computerSelection === "paper"):
-            console.log("You win! " + formatMove(selection) + " beats " + formatMove(computerSelection) + "!")
+        case (selection === "scissors" && computerSelection === "paper"):
+            outputLog("You win! " + formatMove(selection) + " beats " + formatMove(computerSelection) + "!")
             return 1
-        case(selection === "paper" && computerSelection === "paper"):
-            console.log(formatMove(selection) + " vs. " + formatMove(computerSelection) + "! It's a tie!")
+        case (selection === "paper" && computerSelection === "paper"):
+            outputLog(formatMove(selection) + " vs. " + formatMove(computerSelection) + "! It's a tie!")
             return 0
-        case(selection === "paper" && computerSelection === "rock"):
-            console.log("You win! " + formatMove(selection) + " beats " + formatMove(computerSelection) + "!")
+        case (selection === "paper" && computerSelection === "rock"):
+            outputLog("You win! " + formatMove(selection) + " beats " + formatMove(computerSelection) + "!")
             return 1
         default:
-            console.log("Awww, you lost! " + formatMove(computerSelection) + " beats your " + formatMove(selection) + "!")
+            outputLog("Awww, you lost! " + formatMove(computerSelection) + " beats your " + formatMove(selection) + "!")
             return -1
     }
 }
 
-function game() {
-    let userScore = 0
-    let computerScore = 0
-    for (let i = 0; i < 5; i++) {
+var userScore = 0
+var computerScore = 0
+var turns = 0
+
+function playRound() {
+    var action = this.dataset.action
+    let score = runGame(action, computerPlay())
+    turns += 1;
+    if (score < 0) {
+        computerScore += 1
+    } else {
+        userScore += score
+    }
+    /* for (let i = 0; i < 5; i++) {
         let score = runGame(prompt("Enter your move: "), computerPlay())
         if (score < 0) {
             computerScore += 1
         } else {
             userScore += score
         }
+    } */
+    outputLog("Score: " + userScore + "-" + computerScore)
+    if (userScore == 5) {
+        outputLog("You won!")
+        const actions = document.querySelectorAll('.action');
+        actions.forEach(action => action.removeEventListener('click', playRound));
+        actions.forEach(action => action.classList.add("disabled"));
+    } else if (computerScore == 5) {
+        outputLog("You lost!")
+        const actions = document.querySelectorAll('.action');
+        actions.forEach(action => action.removeEventListener('click', playRound));
+        actions.forEach(action => action.classList.add("disabled"));
     }
-    if (userScore == computerScore) {
-        console.log("Score: " + userScore + "-" + computerScore + ". Wow, it's a tied game!")
-    } else if (userScore > computerScore) {
-        console.log("Score: " + userScore + "-" + computerScore + ". Congrats! You win!")
-    } else {
-        console.log("Score: " + userScore + "-" + computerScore + ". Sorry, you lost this time!")
-    }
+    outputLog("-------- [Turn " + turns + "] --------")
 }
 
-game()
+const actions = document.querySelectorAll('.action');
+actions.forEach(action => action.addEventListener('click', playRound));
